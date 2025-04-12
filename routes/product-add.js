b6fe3b66cb8a3e625e6b1144f7b95cbe4485468c
Vizanardi <mzanardiocampo@udesa.edu.ar>
2025-04-12 17:34:17 -0300

    saque los docs que no sirven (con Sofia)

--- routes/product-add.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router() 
-const productAController = require("../controllers/productAController") /*--> Aca lo estamos llamando para usarlo*/
-
-router.get("/", productA.index)
-
-module.exports = router /*esto va siempre al final del docs*/
