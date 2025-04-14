54e71ed153ea799afad57eb236a3683bf9b96ff5
Vizanardi <mzanardiocampo@udesa.edu.ar>
2025-04-14 11:16:06 -0300

    Agregamos el SQL y modificamos algunas cosas de los bucles

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const data = require('../db/index')
 const productController = {
     index: function(req, res){ /*una ruta que envia los datos al navegador*/
-    res.render("product", {productos: data.productos[7]})},
+    res.render("product", {productos: data.productos[1] })},
     }
 
 module.exports = productController
